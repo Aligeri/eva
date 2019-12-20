a7d5b09fecd0e79a784c765d1fdfc2a524892b62
and <testermail35@gmail.com>
2019-12-20 17:21:09 +0300

    поправил 1742, 1743, 1744

--- Pages/AddWalletsPage.py
+++ Pages/AddWalletsPage.py
@@ -45,6 +45,18 @@ class AddWalletsPage(Page):
         self.wait_and_click(add_locator)
         self.wait_and_click(my_wallets_locator)
 
+    def delete_coin(self, delete_locator, my_wallets_locator):
+        """
+        удаляет валюту
+        :param delete_locator: локатор иконки валюты в add wallets
+        :param my_wallets_locator: локатор в my wallets, где должны увидеть что валюта убралась
+        :return:
+        """
+        self.wait_and_click(delete_locator)
+        self.wait_until_element_visible(MyWallets.PupupDontShowAgain)
+        self.wait_and_click_element_within_element(MyWallets.PupupDontShowAgain, MyWallets.GotItButton)
+        self.wait_until_element_invisible(my_wallets_locator, 1)
+
     def delete_from_tokens(self, delete_locator, my_wallets_locator):
         """
         удаляет валюту со вкладки Tokens
@@ -54,10 +66,7 @@ class AddWalletsPage(Page):
         """
         self.wait_and_click(AddWalletsElements.AddWalletsButton)
         self.navigate_to_tokens()
-        self.wait_and_click(delete_locator)
-        self.wait_until_element_visible(MyWallets.PupupDontShowAgain)
-        self.wait_and_click_element_within_element(MyWallets.PupupDontShowAgain, MyWallets.GotItButton)
-        self.wait_until_element_invisible(my_wallets_locator, 1)
+        self.delete_coin(delete_locator, my_wallets_locator)
 
     def delete_from_coins(self, delete_locator, my_wallets_locator):
         """
@@ -68,10 +77,7 @@ class AddWalletsPage(Page):
         """
         self.wait_and_click(AddWalletsElements.AddWalletsButton)
         self.navigate_to_coins()
-        self.wait_and_click(delete_locator)
-        self.wait_until_element_visible(MyWallets.PupupDontShowAgain)
-        self.wait_and_click_element_within_element(MyWallets.PupupDontShowAgain, MyWallets.GotItButton)
-        self.wait_until_element_invisible(my_wallets_locator, 1)
+        self.delete_coin(delete_locator, my_wallets_locator)
 
     def delete_from_featured(self, delete_locator, my_wallets_locator):
         """
@@ -81,7 +87,4 @@ class AddWalletsPage(Page):
         :return:
         """
         self.wait_and_click(AddWalletsElements.AddWalletsButton)
-        self.wait_and_click(delete_locator)
-        self.wait_until_element_visible(MyWallets.PupupDontShowAgain)
-        self.wait_and_click_element_within_element(MyWallets.PupupDontShowAgain, MyWallets.GotItButton)
-        self.wait_until_element_invisible(my_wallets_locator, 1)
+        self.delete_coin(delete_locator, my_wallets_locator)
