a255b49b97eb556034aa21b2e4d35fff7beea754
solinichenko <madokamelpo@gmail.com>
2019-12-23 15:03:04 +0300

    Небольшой рефакторинг тестов по Add Wallets

--- Tests/test_AddWallets.py
+++ Tests/test_AddWallets.py
@@ -39,9 +39,10 @@ class TestClass:
     @xray("QA-1693")
     def test_add_wallets_currencies_availability(self, driver):
         loginPage = LoginPage(driver)
+        addWalletsPage = AddWalletsPage(driver)
         loginPage.login_as_basic_user(UserforCheckAddWallets.email, UserforCheckAddWallets.password)
         loginPage.input_pincode_login(UserforCheckAddWallets.pincode)
-        addWalletsPage = AddWalletsPage(driver)
+        addWalletsPage.wait_until_element_visible(AddWalletsElements.AddWalletsButton, 30)
         addWalletsPage.wait_and_click(AddWalletsElements.AddWalletsButton)
         addWalletsPage.wait_until_element_visible(Featured.FeaturedCardClickable)
         addWalletsPage.navigate_to_coins()
@@ -53,27 +54,30 @@ class TestClass:
     def test_add_wallet_from_featured(self, driver, clear_added_wallets_fixture):
         clear_added_wallets_fixture(user=UserforAddWalletFromFeatured.email)
         loginPage = LoginPage(driver)
+        addWalletsPage = AddWalletsPage(driver)
         loginPage.login_as_basic_user(UserforAddWalletFromFeatured.email, UserforCheckAddWallets.password)
         loginPage.input_pincode_login(UserforAddWalletFromFeatured.pincode)
-        addWalletsPage = AddWalletsPage(driver)
+        addWalletsPage.wait_until_element_visible(AddWalletsElements.AddWalletsButton, 30)
         addWalletsPage.add_from_featured(Featured.FeaturedXEMClicable, MyWallets.XEM)
 
     @xray("QA-1696")
     def test_add_wallet_from_coins(self, driver, clear_added_wallets_fixture):
         clear_added_wallets_fixture(user=UserforAddWalletFromCoins.email)
         loginPage = LoginPage(driver)
+        addWalletsPage = AddWalletsPage(driver)
         loginPage.login_as_basic_user(UserforAddWalletFromCoins.email, UserforAddWalletFromCoins.password)
         loginPage.input_pincode_login(UserforAddWalletFromCoins.pincode)
-        addWalletsPage = AddWalletsPage(driver)
+        addWalletsPage.wait_until_element_visible(AddWalletsElements.AddWalletsButton, 30)
         addWalletsPage.add_from_coins(Coins.CoinsDOGEClicable, MyWallets.DOGE)
 
     @xray("QA-1697")
     def test_add_wallet_from_tokens(self, driver, clear_added_wallets_fixture):
         clear_added_wallets_fixture(user=UserforAddWalletFromTokens.email)
         loginPage = LoginPage(driver)
+        addWalletsPage = AddWalletsPage(driver)
         loginPage.login_as_basic_user(UserforAddWalletFromTokens.email, UserforAddWalletFromTokens.password)
         loginPage.input_pincode_login(UserforAddWalletFromTokens.pincode)
-        addWalletsPage = AddWalletsPage(driver)
+        addWalletsPage.wait_until_element_visible(AddWalletsElements.AddWalletsButton, 30)
         addWalletsPage.add_from_tokens(Tokens.TokensUSDCClicable, MyWallets.USDC)
 
     @xray("QA-1698", "QA-1700")
@@ -82,6 +86,7 @@ class TestClass:
         loginPage = LoginPage(driver)
         loginPage.login_as_basic_user(UserforCheckDeletePopup.email, UserforCheckDeletePopup.password)
         loginPage.input_pincode_login(UserforCheckDeletePopup.pincode)
+        loginPage.wait_until_element_visible(AddWalletsElements.AddWalletsButton, 30)
         loginPage.wait_and_click_element_within_element(MyWallets.DOGE, MyWallets.IconTrash)
         loginPage.wait_until_element_visible(MyWallets.PupupDontShowAgain)
         loginPage.wait_and_click_element_within_element(MyWallets.PupupDontShowAgain, MyWallets.GotItButton)
@@ -99,40 +104,40 @@ class TestClass:
         loginPage.wait_and_click_element_within_element(MyWallets.PupupDontShowAgain, MyWallets.GotItButton)
         loginPage.wait_and_click_element_within_element(MyWallets.XEM, MyWallets.IconTrash)
         loginPage.wait_until_element_invisible(MyWallets.PupupDontShowAgain,1)
-        loginPage.wait_until_element_invisible(MyWallets.XEM)
+        loginPage.wait_until_element_invisible(MyWallets.XEM, 2)
 
     @xray("QA-1742")
     def test_check_delete_from_tokens(self, driver, prepare_for_delete_fixture):
         prepare_for_delete_fixture(user=UserforDeleteFromTokens.email)
         loginPage = LoginPage(driver)
+        addWalletsPage = AddWalletsPage(driver)
         loginPage.login_as_basic_user(UserforDeleteFromTokens.email, UserforDeleteFromTokens.password)
         loginPage.input_pincode_login(UserforDeleteFromTokens.pincode)
-        addWalletsPage = AddWalletsPage(driver)
         addWalletsPage.delete_from_tokens(Tokens.TokensUSDCClicable, MyWallets.USDC)
 
     @xray("QA-1743")
     def test_check_delete_from_coins(self, driver, prepare_for_delete_fixture):
         prepare_for_delete_fixture(user=UserforDeleteFromCoins.email)
         loginPage = LoginPage(driver)
+        addWalletsPage = AddWalletsPage(driver)
         loginPage.login_as_basic_user(UserforDeleteFromCoins.email, UserforDeleteFromCoins.password)
         loginPage.input_pincode_login(UserforDeleteFromCoins.pincode)
-        addWalletsPage = AddWalletsPage(driver)
         addWalletsPage.delete_from_coins(Coins.CoinsDOGEClicable, MyWallets.DOGE)
 
     @xray("QA-1744")
     def test_check_delete_from_featured(self, driver, prepare_for_delete_fixture):
         prepare_for_delete_fixture(user=UserforDeleteFromFeatured.email)
         loginPage = LoginPage(driver)
+        addWalletsPage = AddWalletsPage(driver)
         loginPage.login_as_basic_user(UserforDeleteFromFeatured.email, UserforDeleteFromFeatured.password)
         loginPage.input_pincode_login(UserforDeleteFromFeatured.pincode)
-        addWalletsPage = AddWalletsPage(driver)
         addWalletsPage.delete_from_featured(Featured.FeaturedXEMClicable, MyWallets.XEM)
 
     @xray("QA-1706")
-    def test_search(self, driver, clear_added_wallets_fixture):
+    def test_search_in_add_wallets(self, driver, clear_added_wallets_fixture):
         clear_added_wallets_fixture(user=UserforTestSearchAddWallets.email)
         loginPage = LoginPage(driver)
+        addWalletsPage = AddWalletsPage(driver)
         loginPage.login_as_basic_user(UserforTestSearchAddWallets.email, UserforTestSearchAddWallets.password)
         loginPage.input_pincode_login(UserforTestSearchAddWallets.pincode)
-        addWalletsPage = AddWalletsPage(driver)
         addWalletsPage.test_search_input()
